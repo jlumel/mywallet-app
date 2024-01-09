2e6d4cd76e829954ddd1033e67621487a1859b36
Joaquin Lumelsky <jlumelsky@gmail.com>
2024-01-08 23:47:26 -0300

    Changed baseURL

--- src/utils.js
+++ src/utils.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const fetchAPI = async (method, url, body) => {
 
   const instance = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'https://my-wallet-api-ten.vercel.app',
     withCredentials: true
   })
 
